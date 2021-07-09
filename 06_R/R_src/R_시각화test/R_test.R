97141babf5086c69c27c0ea0a1b83aecb82c6af3
yeonju01 <kee0100@naver.com>
2021-07-10 00:08:34 +0900

    07-10

---
+++ 06_R/R_src/R_시각화test/R_test.R
@@ -0,0 +1,125 @@
+#### test ####
+# 11.datasets::cars데이터 셋을 이용하여 속도에 대한 제동거리의 산점도와 적합도(신뢰구간
+# 그래프)를 나타내시오(단, 속도가 5부터 20까지 제동거리 0부터 100까지만 그래프에 나타냄).
+library(ggplot2)
+install.packages("car")
+library(car)
+str(cars)
+ggplot(cars, aes(x=speed, y=dist)) +
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  coord_cartesian(ylim=c(0,100), xlim=c(5,20))
+ggsave('outData/ex11.png')
+
+# 12.gapminder::gapminder 데이터 셋을 이용하여 1인당국내총생산에 대한 기대수명의 산점
+# 도를 대륙별 다른 색으로 나타내시오.
+library(gapminder)
+str(gapminder)
+ggplot(gapminder, aes(x=gdpPercap, y=lifeExp, col=continent)) +
+  geom_point()
+ggsave('outData/ex12.png')
+
+# 13. gapminder::gapminder 데이터 셋을 이용하여 기대 수명이 70을 초과하는 데이터에 대해
+# 대륙별 데이터 개수
+library(dplyr)
+ggplot(gapminder, aes(x=continent, y=lifeExp)) +
+  geom_col()
+str(gapminder)
+table(gapminder$continent, gapminder$lifeExp>70)
+subset(gapminder, gapminder$lifeExp>70) 
+gapminder %>% 
+  group_by(continent, lifeExp) %>% 
+  subset(lifeExp>70) %>% 
+  summarise(n = n()) %>% 
+  ggplot(aes(x=continent, y=n, fill=continent)) +
+  geom_col() +
+  labs(title = '기대수명이 70을 초과하는 데이터빈도(대륙별)')
+ggsave('outData/ex13.png')
+
+# 14. gapminder::gapminder 데이터 셋을 이용하여 기대수명이 70을 초과하는 데이터에 대해
+# 대륙별 나라 갯수.
+gapminder %>% 
+  group_by(continent, country) %>% 
+  subset(lifeExp>70) %>% 
+  summarise(n = n()) %>% 
+  group_by(continent) %>% 
+  summarise(cnt= n()) %>% 
+  ggplot(aes(x=continent, y=cnt, fill=continent)) +
+  geom_col() +
+  labs(title = '기대수명이 70을 초과하는 대륙별 나라 빈도') +
+  coord_cartesian(ylim=c(0,30))
+ggsave('outData/ex14.png')
+
+# 15. gapminder::gapminder 데이터 셋을 이용하여 대륙별 2007년도(저번에 이부분 뺌) 기대
+# 수명의 사분위수를 시각화
+head(gapminder)
+subset(gapminder, gapminder$year==2007) 
+gapminder %>% 
+  group_by(continent, lifeExp) %>% 
+  subset(year==2007) %>% 
+  ggplot(aes(x=continent, y=lifeExp, col=continent)) +
+  geom_boxplot() +
+  labs(title = '대륙별 기대수명의 사분위수')
+ggsave('outData/ex15.png')
+
+# 16. gapminder::gapminder 데이터 셋을 이용하여 년도별로 gdp와 기대수명과의 관계를 산
+# 점도를 그리고 대륙별 점의 색상을 달리 시각화
+ggplot(gapminder, aes(x=gdpPercap, y=lifeExp, col=continent)) +
+  geom_point() +
+  facet_wrap(~year) +
+  labs(title = 'GDP와 기대수명과의 관계')+
+  coord_cartesian(xlim=c(0,40000))
+ggsave('outData/ex16.png')
+
+# 17. gapminder::gapminder 데이터 셋에서 북한과 한국의 년도별 GDP 변화를 산점도로 시각
+# 화하시오(북한:Korea, Dem. Rep. 한국:Korea, Rep. substr(str, start, end)함수 이용)
+gapminder %>% 
+  group_by(country) %>% 
+  subset(country==c('Korea, Dem. Rep.', 'Korea, Rep.')) %>% 
+  ggplot(aes(x=year, y=gdpPercap, col=country)) +
+  geom_point(pch=3) +
+  labs(subtitle = 'GDP의 변화(한국과 북한)')
+substr(gapminder$country, 8,10)=='Rep'
+ggsave('outData/ex17.png')
+
+# 18. gapminder::gapminder 데이터 셋을 이용하여 한중일 4개국별 GDP 변화를 산점도와 추
+# 세선으로 시각화 하시오.
+country1 <- subset(gapminder, country=='Japan')
+country2 <- subset(gapminder, country=='China')
+country3 <- subset(gapminder, country=='Korea, Rep.')
+country4 <- subset(gapminder, country=='Korea, Dem. Rep.')
+all.country <- rbind(country1, country2, country3, country4)
+all.country %>% 
+  group_by(country) %>% 
+  ggplot(aes(x=year, y=gdpPercap, col=country)) +
+  geom_point() +
+  geom_line()+
+  labs(subtitle = '한중일 4개국의 GDP변화의 산점도와 추세선')
+ggsave('outData/ex18.png')
+
+# 19. gapminder::gapminder 데이터 셋에서 한중일 4개국별 인구변화 변화를 산점도와 추세선
+# 으로 시각화 하시오(scale_y_continuous(labels = scales::comma)추가시 우측처럼)
+country1 <- subset(gapminder, country=='Japan')
+country2 <- subset(gapminder, country=='China')
+country3 <- subset(gapminder, country=='Korea, Rep.')
+country4 <- subset(gapminder, country=='Korea, Dem. Rep.')
+all.country <- rbind(country1, country2, country3, country4)
+ggplot(all.country, aes(x=year, y=pop, col=country)) +
+  geom_point() +
+  geom_line()+
+  labs(subtitle = '한중일 4개국의 인구 변회의 산점도와 추세선')
+ggsave('outData/ex19.png')
+
+# 20. Ggplot2::economic 데이터 셋의 개인 저축률(psavert)가 시간에 따라 어떻게 변해 왔는
+# 지 알아보려 한다. 시간에 따른 개인 저축률의 변화를 나타낸 시계열 그래프와 추세선을 시각
+# 화하시오
+head(economics)
+ggplot(economics, aes(x=date, y=psavert)) +
+  geom_line(col='red', lwd=3) +
+  geom_smooth(col='red') +
+  labs(subtitle = '개인저축률 시계열 그래프')
+ggsave('outData/ex20.png')
+
+# 21. Ggplot2::economic 데이터 셋의 개인 저축률(psavert)과 실업의이 시간에 따라 서로 어
+# 떻게 변해 왔는지 알아보려 한다. 시간에 따른 개인 저축률과 실업률의 변화를 한 그래프에 중
+# 첩하여 시각화하시오
